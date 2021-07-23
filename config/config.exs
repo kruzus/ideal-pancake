6cb57242a41d7e95e2af96b43f8700fb8efdc09e
Bek <kruzus@live.com>
2021-07-23 02:55:16 -0400

    first init

---
+++ config/config.exs
@@ -0,0 +1,32 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :pots,
+  ecto_repos: [Pots.Repo],
+  generators: [binary_id: true]
+
+# Configures the endpoint
+config :pots, PotsWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "2K5Ro/Rhi5x6AyguZ4WpSTcTVFjoTnQtXGBED1LubUOPOiHxk33QfSEeii6pcBwc",
+  render_errors: [view: PotsWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: Pots.PubSub,
+  live_view: [signing_salt: "9evFrjYM"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
